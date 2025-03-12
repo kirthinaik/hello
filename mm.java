970118b59baab264c285b519c37999eda72723ff
kirthinaik <klsvppcs23kirthinaik@gmail.com>
2025-03-12 16:07:53 +0530

    Update and rename mm.py to mm.java

---
+++ mm.java
@@ -0,0 +1,7 @@
+public class mm
+{
+  public static void main(string[args])
+  {
+    system.out.println("hello everyone")
+      }
+}
--- mm.py
+++
@@ -1,4 +0,0 @@
-x=10
-y=20
-z=x+y
-print("the addition of x and y is:",z)
